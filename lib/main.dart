0311c872fe66f94a45e947e26b1a3a075c33385d
Matthew Montero <99379541+Montero-MD@users.noreply.github.com>
2023-11-23 12:16:09 +0800

    PeraPlan Backend

---
+++ lib/main.dart
@@ -0,0 +1,216 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:peraplan/styles.dart';
+import 'database.dart'; // Import the backend code
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransactionAdapter());
+  await Hive.openBox<Transaction>('transactions');
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TransactionScreen(),
+    );
+  }
+}
+
+class TransactionScreen extends StatefulWidget {
+  @override
+  _TransactionScreenState createState() => _TransactionScreenState();
+}
+
+class _TransactionScreenState extends State<TransactionScreen> {
+  late Box<Transaction> _transactionBox;
+  TextEditingController _amountController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  TimeOfDay _selectedTime = TimeOfDay.now();
+  String? _selectedCategory;
+  String? _selectedTransactionType;
+
+  @override
+  void initState() {
+    super.initState();
+    _transactionBox = Hive.box<Transaction>('transactions');
+  }
+
+  void _addTransaction() {
+    // Parse the amount from the string to double
+    double parsedAmount = double.tryParse(_amountController.text) ?? 0.0;
+
+    Transaction newTransaction;
+
+    if (_selectedTransactionType == 'Pera In') {
+      newTransaction = PeraIn(
+        amount: parsedAmount,
+        date: _selectedDate,
+        time: _selectedTime,
+        category: _selectedCategory,
+      );
+    } else if (_selectedTransactionType == 'Pera Out') {
+      newTransaction = PeraOut(
+        amount: parsedAmount,
+        date: _selectedDate,
+        time: _selectedTime,
+        category: _selectedCategory,
+      );
+    } else {
+      // Handle other cases or throw an error if needed
+      return;
+    }
+
+    _transactionBox.add(newTransaction);
+    _amountController.clear();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transaction App'),
+      ),
+      body: Column(
+        children: [
+          // Dropdown for Transaction Type
+          DropdownButtonFormField<String>(
+            value: _selectedTransactionType,
+            items: ['Pera In', 'Pera Out']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              if (newValue != null) {
+                setState(() {
+                  _selectedTransactionType = newValue;
+                });
+              }
+            },
+            decoration: InputDecoration(labelText: 'Transaction Type'),
+          ),
+          SizedBox(height: medium),
+
+          TextFormField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Amount'),
+          ),
+          SizedBox(height: xsmall),
+
+          Row(
+            children: [
+              Text('Date:'),
+              SizedBox(width: xsmall),
+              ElevatedButton(
+                onPressed: () async {
+                  final pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: _selectedDate,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null && pickedDate != _selectedDate) {
+                    setState(() {
+                      _selectedDate = pickedDate;
+                    });
+                  }
+                },
+                child: Text('Select Date'),
+              ),
+              SizedBox(width: xsmall),
+              Text('${DateFormat('yyyy-MM-dd').format(_selectedDate)}')
+            ],
+          ),
+          SizedBox(height: xsmall),
+          Row(
+            children: [
+              Text('Time:'),
+              SizedBox(width: xsmall),
+              ElevatedButton(
+                onPressed: () async {
+                  final TimeOfDay? timeOfDay = await showTimePicker(
+                    context: context,
+                    initialTime: _selectedTime,
+                    initialEntryMode: TimePickerEntryMode.dial,
+                  );
+                  if (timeOfDay != null) {
+                    setState(() {
+                      _selectedTime = timeOfDay;
+                    });
+                  }
+                },
+                child: Text('Select Time'),
+              ),
+              SizedBox(width: xsmall),
+              Text(
+                  '${_selectedTime.format(context)}') // Display the selected time
+            ],
+          ),
+          SizedBox(height: xsmall),
+
+          DropdownButtonFormField<String>(
+            value: _selectedCategory,
+            items: ['Food', 'Transportation', 'Shopping', 'Others']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              if (newValue != null) {
+                setState(() {
+                  _selectedCategory = newValue;
+                });
+              }
+            },
+            decoration: InputDecoration(labelText: 'Category'),
+          ),
+          ElevatedButton(
+            onPressed: _addTransaction,
+            child: Text('Add Transaction'),
+          ),
+          Expanded(
+            child: ValueListenableBuilder(
+              valueListenable: _transactionBox.listenable(),
+              builder: (context, Box<Transaction> box, _) {
+                return ListView.builder(
+                  itemCount: box.length,
+                  itemBuilder: (context, index) {
+                    final transaction = box.getAt(index);
+
+// Determine the transaction type
+                    String transactionType = '';
+                    if (transaction is PeraIn) {
+                      transactionType = 'Pera In';
+                    } else if (transaction is PeraOut) {
+                      transactionType = 'Pera Out';
+                    }
+
+                    return ListTile(
+                      title: Text('${transactionType}'),
+                      subtitle: Text(
+                          'Amount: ${transaction?.amount}\nDate: ${DateFormat('yyyy-MM-dd').format(transaction?.date ?? DateTime.now())}\nTime: ${transaction?.time ?? TimeOfDay.now()}\nCategory: ${transaction?.category}'),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/styles.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Colors
+var hlblue = const Color(0xFF0CC0DF);
+var text = const Color(0xFF0FA3B1);
+var icon = const Color(0xFFB8BCCB);
+var white = const Color(0xFFFFFFFF);
+var black = const Color(0xFF000000);
+var gray = const Color(0xFFE3E3E3);
+var dgray = const Color(0x65000000);
+var lgray = const Color(0x35E3DCDC);
+var green = const Color(0xFF0FB13C);
+var red = const Color(0xFFB1360F);
+
+// TextStyles
+TextStyle titlePera = TextStyle(
+    fontFamily: 'Batangas',
+    fontSize: 64,
+    fontWeight: FontWeight.w700,
+    color: hlblue);
+TextStyle titlePlan = TextStyle(
+    fontFamily: 'Batangas',
+    fontSize: 64,
+    fontWeight: FontWeight.w700,
+    color: text);
+TextStyle subPera = TextStyle(
+    fontFamily: 'Batangas',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: hlblue);
+TextStyle subPlan = TextStyle(
+    fontFamily: 'Batangas',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: text);
+TextStyle headers = GoogleFonts.lexend(
+    fontSize: 20, fontWeight: FontWeight.w700, color: hlblue);
+TextStyle subHeaders = GoogleFonts.lexend(
+    fontSize: 15, fontWeight: FontWeight.w700, color: hlblue);
+TextStyle hltxt = GoogleFonts.lexend(
+    fontSize: 11, fontWeight: FontWeight.w700, color: hlblue);
+TextStyle transactxt = GoogleFonts.lexend(
+    fontSize: 16, fontWeight: FontWeight.w400, color: hlblue);
+TextStyle inpAmt =
+    GoogleFonts.lexend(fontSize: 32, fontWeight: FontWeight.w700, color: white);
+TextStyle outAmt =
+    GoogleFonts.lexend(fontSize: 20, fontWeight: FontWeight.w700, color: red);
+
+TextStyle pIn =
+    GoogleFonts.lexend(fontSize: 24, fontWeight: FontWeight.w700, color: green);
+TextStyle pOut =
+    GoogleFonts.lexend(fontSize: 24, fontWeight: FontWeight.w700, color: red);
+TextStyle pText = GoogleFonts.lexend(
+    fontSize: 18, fontWeight: FontWeight.w400, color: hlblue);
+TextStyle uName =
+    GoogleFonts.lexend(fontSize: 24, fontWeight: FontWeight.w700, color: white);
+TextStyle balAmt =
+    GoogleFonts.lexend(fontSize: 32, fontWeight: FontWeight.w600, color: white);
+TextStyle hintAmt =
+    GoogleFonts.lexend(fontSize: 24, fontWeight: FontWeight.w700, color: white);
+TextStyle transacNormal = GoogleFonts.lexend(
+    fontSize: 24, fontWeight: FontWeight.w400, color: hlblue);
+TextStyle transacBold = GoogleFonts.lexend(
+    fontSize: 24, fontWeight: FontWeight.w700, color: hlblue);
+
+TextStyle tIn =
+    GoogleFonts.lexend(fontSize: 14, fontWeight: FontWeight.w600, color: green);
+TextStyle tOut =
+    GoogleFonts.lexend(fontSize: 14, fontWeight: FontWeight.w600, color: red);
+TextStyle tCat = GoogleFonts.lexend(
+    fontSize: 14, fontWeight: FontWeight.w600, color: hlblue);
+TextStyle txt =
+    GoogleFonts.lexend(fontSize: 15, fontWeight: FontWeight.w400, color: dgray);
+
+TextStyle lNormal = GoogleFonts.lexend(
+    fontSize: 22, fontWeight: FontWeight.w400, color: hlblue);
+TextStyle lBold = GoogleFonts.lexend(
+    fontSize: 22, fontWeight: FontWeight.w700, color: hlblue);
+
+TextStyle subGreen =
+    GoogleFonts.lexend(fontSize: 14, fontWeight: FontWeight.w600, color: green);
+TextStyle subRed =
+    GoogleFonts.lexend(fontSize: 14, fontWeight: FontWeight.w600, color: red);
+
+TextStyle tagline = GoogleFonts.lexend(
+    fontSize: 12, fontWeight: FontWeight.normal, color: text);
+
+// Gaps
+var xxxlarge = 200.0;
+var xxlarge = 150.0;
+var xlarge = 100.0;
+var large = 50.0;
+var medium = 30.0;
+var small = 16.0;
+var xsmall = 10.0;
+var xxsmall = 5.0;
+var cont = 330.0;
